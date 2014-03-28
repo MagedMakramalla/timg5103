7ee41fe98143cd925299d3eb9150573735e1bcf8
vignesh <vignesh@ubuntu.(none)>
2014-03-28 11:34:58 -0700

    Find basic connections

--- script.py
+++ script.py
@@ -1,57 +1,51 @@
-# import sys
-# 
-# 
-# 
-# def main():
-#     
-#     
-#     user = maged
-#     desired_contact = vignesh
-#     num_of_connections = 5
-#     
-#     i = 0
-#     
-#     degree_of_seperaation=0
-#     
-#     contacts_in_link= []
-#     
-#     stopping_value = 10000
-#     
-#     searched_in = user  
-#     
-#     not_found = false
-#     found  = true
-#     
-#     for num1 in [0,stopping_value]:
-#         
-#         if found==true:
-#             break
-#         
-#         if not_found==true:
-#             searched_in = searched_in.friends(i)
-#             contact_in_link[degree_of_seperaation] = searched_in
-#             i++
-#             
-#             not_found==false
-#             
-#         for num2 in [0, searched_in.numoffriends]:
-#             if searched_in.friends(num2) == desired_contact:
-#                 found = true
-#                 break 
-#          
-#         not_found==true
-#          
-#         if i == searched_in.numoffriends:
-#             i=0
-#             degree_of_seperaation++
-#          
-#     
-#     if found==true:
-#         print 'you found your contact with' + degree_of_seperation + 'degree of seperation'
-#         
-#         print 'you can connect to' +  desired_contact + 'through' + contact_in_link
-#         
-#     else:
-#         print 'no contact found' 
-#     
-#         
+
+import sys
+import requests
+ 
+def get_user(name):
+    resp = requests.get('http://127.0.0.1:9010/getUser?username=%s' % (name))
+        
+    resp_data = resp.json()
+    return resp_data
+
+
+
+def find_connection():
+        
+    user = 'vignesh'
+    desired_contact = 'asdsad'
+    look_up_in = user
+    
+    i = 0       
+    degree_of_seperation = 0
+    contacts_in_link= []    
+    stopping_value = 50    
+    found  = False
+    
+    while not found:        
+        user_data = get_user(look_up_in)            
+        for each_connection in user_data['connections']:            
+            if each_connection['name'] == desired_contact:
+                found = True
+                break        
+            i += 1        
+        if not found:
+            look_up_in = user_data['connections'][0]['name']
+            contacts_in_link.append(look_up_in)
+            degree_of_seperation += 1
+        if i >= stopping_value:
+            break
+        
+                 
+    if found:
+        print 'you found your contact with ' + str(degree_of_seperation) + ' degree of seperation'
+        if degree_of_seperation == 0:
+            print 'This is your direct connection'
+        else:
+            print 'you can connect to ' +  desired_contact + ' through ' + ','.join(contacts_in_link)         
+    else:
+        print 'no connection found' 
+     
+         
+find_connection()
+
